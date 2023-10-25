ac9657472186d67bb156e8f0eb067deb45a9e9e8
Uf11 <itsusman11@gmail.com>
2023-10-25 22:35:40 +0500

    Task 1 Problem 1

--- app/src/main/java/com/example/task2/MainActivity.kt
+++ app/src/main/java/com/example/task2/MainActivity.kt
@@ -19,6 +19,50 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.TextStyle
 import com.example.task2.ui.theme.Task2Theme
 
+@Composable
+fun Task1Problem1(){
+    Column (modifier = Modifier.background(Color.White)){
+        Image(painter = painterResource(R.drawable.bg_task1p1),
+            contentDescription = null)
+        Text(
+            text = "Jetpack Compose tutorial",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            modifier = Modifier.padding(16.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+    }
+}
+@Preview
+@Composable
+fun Task1Problem1Preview(){
+    Task2Theme {
+        Task1Problem1()
+    }
+}
+
+@Composable
+fun Task1Problem2(){
+
+}
+
+@Preview
+@Composable
+fun Task1Problem2Preview(){
+    Task2Theme {
+        Task1Problem2()
+    }
+}
+
 data class WeatherData(val day: String, val weatherIcon: Int, val temperature: String)
 val weatherData = listOf(
     WeatherData("MON", R.drawable.ic_cloudy, "75°F"),
@@ -192,7 +236,7 @@ private fun WeatherCardRow()
 }
 
 @Composable
-fun Page1() {
+fun Task2Page1() {
     Column {
         WeatherBox(imgRes = R.drawable.bg_buildings)
         WeatherCardRow()
@@ -201,9 +245,9 @@ fun Page1() {
 
 @Preview
 @Composable
-fun Page1Preview(){
+fun Task2Page1Preview(){
     Task2Theme {
-        Page1()
+        Task2Page1()
     }
 }
 
@@ -351,7 +395,7 @@ private fun CityCardsColumn() {
 }
 
 @Composable
-fun Page2() {
+fun Task2Page2() {
     Column {
         TopCompPage2()
         CityCardsColumn()
@@ -359,14 +403,14 @@ fun Page2() {
 }
 @Preview
 @Composable
-fun Page2Preview() {
+fun Task2Page2Preview() {
     Task2Theme {
-        Page2()
+        Task2Page2()
     }
 }
 
 @Composable
 fun Main(){
-    Page1()
-    Page2()
+    Task2Page1()
+    Task2Page2()
 }
